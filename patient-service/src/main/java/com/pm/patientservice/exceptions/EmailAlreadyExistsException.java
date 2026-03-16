8450510c0e1c4271dd22c46d9f7a124259795c3b
titicodes <anietimfoneeffiong@gmail.com>
2026-03-16 14:16:54 +0100

    first commit

---
+++ patient-service/src/main/java/com/pm/patientservice/exceptions/EmailAlreadyExistsException.java
@@ -0,0 +1,9 @@
+package com.pm.patientservice.exceptions;
+
+public class EmailAlreadyExistsException extends RuntimeException {
+
+    public EmailAlreadyExistsException(String message) {
+        super(message);
+    }
+}
+
---
+++ patient-service/src/main/java/com/pm/patientservice/exceptions/PatientNotFoundException.java
@@ -0,0 +1,9 @@
+package com.pm.patientservice.exceptions;
+
+public class PatientNotFoundException extends RuntimeException {
+
+    public PatientNotFoundException(String message) {
+        super(message);
+    }
+}
+
